42470b6d6cd0c42a1e06a973597ddd487987d03e
Galih Sansabilla <gtampan61@gmail.com>
2020-04-13 21:19:00 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,46 @@
+<?php
+$jumlahDonasi 	= "1000";
+$updateWaktu 	= "13/04/2020 0:00 WIB";
+$speed          = 500; //Semakin kecil semakin cepat
+$mulai 			= date('2020-03-26'); // tanggal mulai
+$akhir 			= date('2020-04-30'); // tanggal berakhir
+
+//jangan edit 
+function penyebut($nilai) {
+	$nilai = abs($nilai);
+	$huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+	$temp = "";
+	if ($nilai < 12) {
+		$temp = " ". $huruf[$nilai];
+	} else if ($nilai <20) {
+		$temp = penyebut($nilai - 10). " belas";
+	} else if ($nilai < 100) {
+		$temp = penyebut($nilai/10)." puluh". penyebut($nilai % 10);
+	} else if ($nilai < 200) {
+		$temp = " seratus" . penyebut($nilai - 100);
+	} else if ($nilai < 1000) {
+		$temp = penyebut($nilai/100) . " ratus" . penyebut($nilai % 100);
+	} else if ($nilai < 2000) {
+		$temp = " seribu" . penyebut($nilai - 1000);
+	} else if ($nilai < 1000000) {
+		$temp = penyebut($nilai/1000) . " ribu" . penyebut($nilai % 1000);
+	} else if ($nilai < 1000000000) {
+		$temp = penyebut($nilai/1000000) . " juta" . penyebut($nilai % 1000000);
+	} else if ($nilai < 1000000000000) {
+		$temp = penyebut($nilai/1000000000) . " milyar" . penyebut(fmod($nilai,1000000000));
+	} else if ($nilai < 1000000000000000) {
+		$temp = penyebut($nilai/1000000000000) . " trilyun" . penyebut(fmod($nilai,1000000000000));
+	}     
+	return $temp;
+}
+
+function terbilang($nilai) {
+	if($nilai<0) {
+		$hasil = "minus ". trim(penyebut($nilai));
+	} else {
+		$hasil = trim(penyebut($nilai));
+	}     		
+	return $hasil;
+}
+?>
+
